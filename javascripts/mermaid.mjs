9b7ca1f7c9f8efe618f18757be2cee5ef09a4ac6
abbemalmo99 <abbemalmo99@users.noreply.github.com>
2025-12-16 08:09:42 +0000

    deploy: 9bf66f88ddc1451e7e30f5d7be83cb12e5a9677c

---
+++ javascripts/mermaid.mjs
@@ -0,0 +1,16 @@
+import mermaid from "https://cdn.jsdelivr.net/npm/mermaid@11/dist/mermaid.esm.min.mjs";
+
+mermaid.initialize({
+  startOnLoad: true,
+  theme: "base",
+  themeVariables: {
+    primaryColor: "#6EAA5B",
+    primaryTextColor: "#01351C",
+    primaryBorderColor: "#01351C",
+    lineColor: "#9ABEAA",
+    secondaryColor: "#D1E0D7",
+    tertiaryColor: "#f0f5f1",
+  },
+});
+
+
